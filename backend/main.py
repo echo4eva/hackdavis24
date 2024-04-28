25d933ae8268175ae407f5725baad2d291d86bef
David Kim <dvkim@ucdavis.edu>
2024-04-27 22:27:36 -0700

    YAYY

--- backend/main.py
+++ backend/main.py
@@ -5,15 +5,17 @@ from client_auth import client
 
 def main():
     # Record audio and save to a file
-   # print("Recording audio")
-  #  record_audio()
-   # print("Done recording audio")
+    print("Recording audio")
+    record_audio()
+    print("Done recording audio")
 
     # Perform speech recognition on the recorded audio
-    #print("Doing speech to text")
-    #recognized_text = recognize_speech()
-    #print(f"Recognized text: {recognized_text}")
-    client.create_tweet(text = "testing", user_auth = True)
+    print("Doing speech to text")
+    recognized_text = recognize_speech()
+    print(f"Recognized text: {recognized_text}")
+    print("Creating Tweet:")
+    client.create_tweet(text = recognized_text, user_auth = True)
+    print("Tweeted")
 
 if __name__ == "__main__":
     main()
