f9696c4b714ec44c674ae20d2fdf4d749f407e0d
David Kim <dvkim@ucdavis.edu>
2024-04-27 22:19:07 -0700

    ughhhhhhh

--- backend/client_auth.py
+++ backend/client_auth.py
@@ -4,10 +4,11 @@ import os
 
 load_dotenv()
 
-client = tweepy_Client(
-    bearer_token = os.getenv('BEARER_TOKEN'),
-    consumer_key = os.getenv('CONSUMER_KEY'),
+client = tweepy.Client(
+  #  bearer_token = os.getenv('BEARER_TOKEN'),
+    consumer_key = os.getenv("CONSUMER_KEY"),
     consumer_secret= os.getenv("CONSUMER_SECRET"),
     access_token= os.getenv("ACCESS_TOKEN"),
     access_token_secret= os.getenv("ACCESS_SECRET"),
 )
+
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,19 @@
 # backend\main.py
 from recorder import main as record_audio
 from speech_recognition import main as recognize_speech
+from client_auth import client
 
 def main():
     # Record audio and save to a file
-    print("Recording audio")
-    record_audio()
-    print("Done recording audio")
+   # print("Recording audio")
+  #  record_audio()
+   # print("Done recording audio")
 
     # Perform speech recognition on the recorded audio
-    print("Doing speech to text")
-    recognized_text = recognize_speech()
-    print(f"Recognized text: {recognized_text}")
+    #print("Doing speech to text")
+    #recognized_text = recognize_speech()
+    #print(f"Recognized text: {recognized_text}")
+    client.create_tweet(text = "testing", user_auth = True)
 
 if __name__ == "__main__":
     main()
