4e9e6deea3b1efb6bce6a8d8325bb9b217dd2350
David Kim <dvkim@ucdavis.edu>
2024-04-27 22:53:35 -0700

    FINALLY

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from recorder import main as record_audio
 from speech_recognition import main as recognize_speech
 from client_auth import client
 
-def main():
+def speech_text(): 
     # Record audio and save to a file
     print("Recording audio")
     record_audio()
@@ -13,9 +13,27 @@ def main():
     print("Doing speech to text")
     recognized_text = recognize_speech()
     print(f"Recognized text: {recognized_text}")
-    print("Creating Tweet:")
-    client.create_tweet(text = recognized_text, user_auth = True)
-    print("Tweeted")
+    return recognized_text
+
+def main():
+    # Record audio and save to a file
+    r_text = speech_text() 
+    bool_variable = False
+
+    while(len(r_text) > 280): 
+        print("For the love of god stop yapping and try again")
+        r_text = speech_text()
+
+    while(bool_variable == False):
+        print("Is this to your liking? ")
+        choice = input("Y or N: ")
+        if(choice == 'Y'):
+            bool_variable = True
+            print("Creating Tweet:")
+            client.create_tweet(text = r_text, user_auth = True)
+            print("Tweeted")
+        else:
+            r_text = speech_text() 
 
 if __name__ == "__main__":
     main()
